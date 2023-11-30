0fbbd1da709f944a9743cf1adff44361b7ae6e5f
Lucas Cesca <lucascesca14@gmail.com>
2023-11-29 21:07:06 -0300

    Implemeting possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -13,8 +13,16 @@ public class Position {
         return row;
     }
 
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
     public Integer getColumn() {
         return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
     }
 
     public void setValues(int row, int column) {
